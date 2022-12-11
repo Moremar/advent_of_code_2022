295b727b1c805275d7403a3707812d5b1b567586
Moremar <chevallier.th@gmail.com>
2022-12-11 15:18:40 +0900

    Day 11 Python

---
+++ python/day11/part1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+class Monkey:
+    def __init__(self, items, op, dest):
+        self.items = items
+        self.op = op
+        self.dest = dest
+        self.inspected = 0
+
+    def __repr__(self):
+        return f'Monkey({self.items}, {self.op}, {self.dest}, {self.inspected})'
+
+
+def parse(input_path):
+    monkeys = []
+    with open(input_path, 'r') as f:
+        for block in f.read().split('\n\n'):
+            lines = block.split('\n')
+            items = list(map(int, lines[1].split(': ')[1].split(', ')))
+            op = 'ADD' if lines[2].split('old ')[1][0] == '+' else 'MUL', lines[2].split('old ')[1][1:].strip()
+            dest = (int(lines[3].split('by')[1]), int(lines[4].split('key')[1]), int(lines[5].split('key')[1]))
+            monkeys.append(Monkey(items, op, dest))
+    return monkeys
+
+
+def solve_part1(monkeys):
+    for _ in range(20):
+        for monkey in monkeys:
+            while len(monkey.items):
+                monkey.inspected += 1
+                item = monkey.items[0]
+                monkey.items = monkey.items[1:]
+                val = item if monkey.op[1] == 'old' else int(monkey.op[1])
+                worry = item + val if monkey.op[0] == 'ADD' else item * val
+                worry //= 3
+                next_monkey = monkey.dest[1] if worry % monkey.dest[0] == 0 else monkey.dest[2]
+                monkeys[next_monkey].items.append(worry)
+    inspected = sorted([monkey.inspected for monkey in monkeys], reverse=True)
+    return inspected[0] * inspected[1]
+
+
+if __name__ == "__main__":
+    parsed = parse("data.txt")
+    print('Part 1 :', solve_part1(parsed))
---
+++ python/day11/part2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import part1
+
+# without the "divide by 3" operation, we get unmanageably big worry levels
+# we do not need to work with these actual numbers, since all we care about to
+# decide which monkey will receive each item is whether or not the number is divisible
+# by each monkey's test value
+# instead of using the actual number, we can just use its remainder modulo LCM
+# (least common multiple of all monkeys' test values)
+# since all monkeys test values are prime, the LCM is the product of distinct values
+
+
+def solve_part2(monkeys):
+    lcm = 1
+    for prime in set([m.dest[0] for m in monkeys]):
+        lcm *= prime
+
+    for i in range(10000):
+        for monkey in monkeys:
+            while len(monkey.items):
+                monkey.inspected += 1
+                item = monkey.items[0]
+                monkey.items = monkey.items[1:]
+                val = item if monkey.op[1] == 'old' else int(monkey.op[1])
+                worry = item + val if monkey.op[0] == 'ADD' else item * val
+                worry = worry % lcm
+                next_monkey = monkey.dest[1] if worry % monkey.dest[0] == 0 else monkey.dest[2]
+                monkeys[next_monkey].items.append(worry)
+    inspected = sorted([monkey.inspected for monkey in monkeys], reverse=True)
+    return inspected[0] * inspected[1]
+
+
+if __name__ == "__main__":
+    parsed = part1.parse("data.txt")
+    print('Part 1 :', solve_part2(parsed))
